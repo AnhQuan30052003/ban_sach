d9805cadc062b50a40284f6acb721887f992fc9e
AnhQuan30052003 <anhquan300503@gmail.com>
2024-11-14 23:22:22 +0700

    add table nha_xuat_ban & tac_gia: unfinish

--- test_code/build_data/main.py
+++ test_code/build_data/main.py
@@ -24,13 +24,20 @@ with open(path, "r") as file:
     data.append(line)
 
 path = os.path.join(pathCur, "output.txt")
-with open(path, "a") as file:
+with open(path, "w") as file:
+  for line in data:
+    file.write(line)
+    file.write("\n")
+
   start = 29
   end = 100
+  end = 200
   index = 0
   for i in range(start, end + 1):
     id = chuoi_so(i)
+    print(id)
     line = "('" + id + data[index][6:]
+    print(line)
     file.write(line)
     file.write("\n")
 
